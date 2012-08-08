c4e427d0bc2fa5a15f606984fb579e319b7905e1
Aaron <aaron.l.france@gmail.com>
2012-08-08 21:12:26 +0200

    Working towards a log in/out system

--- chatapp.erl
+++ chatapp.erl
@@ -1,24 +1,58 @@
 -module(chatapp).
--export([startServer/0,sendMessage/2,server/0,startReceiver/0,receiver/0]).
+-export([startServer/0,sendMessage/2,server/1,login/1,logout/1,receiver/0]).
 
 startServer() ->
     % starts the server and registers it's handle.
-    register(server, spawn(chatapp, server, [])),
+    register(server, spawn(chatapp, server, [orddict:new()])),
     ok.
 
-server() ->
+server(UserList) ->
     % recursive loop which constantly reads it's mailbox to find
-    % messages to send to clients.
+    % instructions from clients.
     receive
-        {message, PID, To,Message} ->
-            io:format("Message for: ~p. With: ~p~n", [PID, Message]),
-            {receiver, To} ! {message, Message},
-            server()
+
+        {message, From, To, Message} ->
+
+	    case orddict:find(From, UserList) of
+		{ok, _ } ->
+		    ok;
+		error ->
+		    {receiver, From} ! {message, "You are not logged in!"},
+		    server(UserList)
+	    end,
+
+	    case orddict:find(To, UserList) of
+		{ok, _ } ->
+		    io:format("Message for: ~p. With: ~p~n", [To, Message]),
+		    {receiver, To} ! {message, Message},
+		    server(UserList);
+		error ->
+		    {receiver, From} ! {message, "Recipient not found!"},
+		    server(UserList)
+	    end.
+
+	{login, From, Who} ->
+	    {receiver, From} ! {message, io_lib:format("Logged in as: ~p~n", [Who])},
+	    server(orddict:append(Who, From, UserList));
+
+	{logout, From, Who} ->
+	    {receiver, From} ! {message, io_lib:format("Logged out from: ~p~n", [Who])},
+	    server(orddict:erase(Who, UserList))
+
+    after
+	10000 ->
+	    io:format("Polling.....~n"),
+	    server(UserList)
     end.
 
-startReceiver() ->
+login(Username) ->
     % starts a client receive channel.
+    {server, server@archbox} ! {login, self(), Username},
     register(receiver, spawn(chatapp, receiver, [])),
+    ok.
+
+logout(Username) ->
+    {server, server@archbox} ! {logout, self(), Username},
     ok.
 
 receiver() ->
@@ -30,8 +64,8 @@ receiver() ->
             receiver()
     end.
 
-sendMessage(Message, To) ->
+sendMessage(To, Message) ->
     % send a message to the server which is intended for a specific
     % recipient.
-    {server, meme@FRANCEA1} ! {message, self(), To, Message},
+    {server, server@archbox} ! {message, self(), To, Message},
     ok.
