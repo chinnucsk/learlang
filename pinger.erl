96718399682679824b9def8980c92bc7b03bc8e9
AeroNotix <aaron.l.france@gmail.com>
2012-08-08 13:33:33 +0200

    Added useful pinger tool. Simple ping/pong response

---
+++ pinger.erl
@@ -0,0 +1,23 @@
+-module(pinger).
+-export([start_pong/0, ping/1, pong/0]).
+
+ping(Node) ->
+    {ponger, Node} ! {ping, self()},
+    receive
+        pong ->
+            io:format("ok", [])
+    after
+        1000 ->
+            io:format("No response!")
+    end.
+
+pong() ->
+    receive
+        {ping, PID} ->
+            io:format("Ping:~p~n", [PID]),
+            PID ! pong,
+            pong()
+    end.
+
+start_pong() ->
+    register(ponger, spawn(pinger, pong, [])).
