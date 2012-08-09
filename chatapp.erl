d41887d9eca845b78201b4f1da0fb9f6183f4e7b
U-EMEA\francea <francea@FRANCEA1.emea.hpqcorp.net>
2012-08-09 11:21:20 +0200

    Logging in/out works and user's status is checked on sendMessage calls

--- chatapp.erl
+++ chatapp.erl
@@ -1,11 +1,11 @@
 -module(chatapp).
 -author("Aaron France").
--export([startServer/0,sendMessage/2,server/1,login/1,logout/1,receiver/0]).
+-export([startServer/0,sendMessage/2,server/2,login/1,logout/1,receiver/0]).
 -define(SERVER, server@FRANCEA1).
 
 %% starts the server and registers it's Pid to server.
 startServer() ->
-    register(server, spawn(chatapp, server, [orddict:new()])),
+    register(server, spawn(chatapp, server, [orddict:new(),orddict:new()])),
     ok.
 
 %% recursive loop which constantly reads it's mailbox to find
@@ -13,43 +13,54 @@ startServer() ->
 %%
 %% We can deal with handling messages to clients and logging
 %% in and out.
-server(UserList) ->
+server(UserListA, UserListB) ->
+
     receive
+        {status, Return, From} ->
+            Return ! orddict:find(From, UserListB),
+            server(UserListA, UserListB);
+
         {message, From, To, Message} ->
-            case orddict:find(To, UserList) of
+            case orddict:find(To, UserListA) of
                 {ok, _ } ->
                     io:format("Message for: ~p. With: ~p~n", [To, Message]),
-                    {getmsg, orddict:fetch(To, UserList)} ! {message, Message},
-                    server(UserList);
+                    {getmsg, orddict:fetch(To, UserListA)} ! {message, Message},
+                    server(UserListA, UserListB);
                 error ->
                     {getmsg, From} ! {message, "Recipient not found!"},
-                    server(UserList)
+                    server(UserListA, UserListB)
             end;
 
         {login, Return, From, Who} ->
-            case orddict:find(Who, UserList) of
+            case orddict:find(Who, UserListA) of
                 {ok, _ } ->
                     Return ! {error, "Already logged in"},
-                    server(UserList);
+                    server(UserListA, UserListB);
                 error ->
                     Return ! {ok, logged_in},
-                    server(orddict:store(Who, From, UserList))
+                    server(
+                      orddict:store(Who, From, UserListA),
+                      orddict:store(From, Who, UserListB)
+                     )
             end;
 
-        {logout, Return, Who} ->
-            case orddict:find(Who, UserList) of
+        {logout, Return, From, Who} ->
+            case orddict:find(Who, UserListA) of
                 {ok, _ } ->
                     Return ! {ok, logged_out},
-                    server(orddict:erase(Who, UserList));
+                    server(
+                      orddict:erase(Who, UserListA),
+                      orddict:erase(From, UserListB)
+                     );
                 error  ->
                     Return ! {error, "You were not logged in!"},
-                    server(UserList)
+                    server(UserListA, UserListB)
             end
 
     after
         10000 ->
             io:format("Polling.....~n"),
-            server(UserList)
+            server(UserListA, UserListB)
     end.
 
 %% Logs a user onto the server.
@@ -71,13 +82,17 @@ login(Username) ->
 
 %% Logs a user out on the server.
 logout(Username) ->
-    {server, ?SERVER} ! {logout, self(), Username},
+    {server, ?SERVER} ! {logout, self(), node(), Username},
+
     receive
         {ok, logged_out} ->
             io:format(logged_out),
             exit(whereis(getmsg), kill);
         {error, Message} ->
             io:format(Message)
+    after
+        3000 ->
+            io:format("Error logging out")
     end,
     ok.
 
@@ -96,5 +111,11 @@ receiver() ->
 sendMessage(To, Message) ->
     % send a message to the server which is intended for a specific
     % recipient.
-    {server, ?SERVER} ! {message, node(), To, Message},
+    {server, ?SERVER} ! {status, self(), node()},
+    receive
+        {ok, _ } ->
+            {server, ?SERVER} ! {message, node(), To, Message};
+        error ->
+            io:format("You are not logged in!")
+    end,
     ok.
